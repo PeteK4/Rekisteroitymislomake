5b36c9e3243c2bcad711c69ee4ecc9a618785a64
PeteK4 <118457821+PeteK4@users.noreply.github.com>
2023-11-07 20:24:15 +0200

    Muokattu

--- javascript/animaatiot.js
+++ javascript/animaatiot.js
@@ -1,37 +1,39 @@
-// Animoi elementit
-document.addEventListener("DOMContentLoaded", function() {
-    const lomake = document.querySelector(".lomake");
-    const animaatioSuunta = document.querySelector('script[src="./javascript/animaatiot.js"]').getAttribute("id");
+const image = new Image();
+image.src = "./paperi.webp";
+
+image.addEventListener("load", function() {
+  const lomake = document.querySelector(".lomake");
+  const animaatioSuunta = document.querySelector('script[src="./javascript/animaatiot.js"]').getAttribute("id");
 
   // Valitaan animaatio
-    if (animaatioSuunta == "vasen>oikea") {
-      vasenOikea(lomake);
-    } else if (animaatioSuunta == "ylhaalta>alas") {
-      ylhaaltaAlas(lomake);
-    }
-  });
-  
-// Siirrä elementti vasemmalta oikealle
-  function vasenOikea(lomake) {
-    const alkupiste = -lomake.offsetWidth;
-    const loppupiste = 100;
-    lomake.style.left = `${alkupiste}px`;
-  
-    setTimeout(() => {
-      lomake.style.transition = "left .8s";
-      lomake.style.left = `${loppupiste}px`;
-    }, 0);
+  if (animaatioSuunta == "vasen>oikea") {
+    vasenOikea(lomake);
+  } else if (animaatioSuunta == "ylhaalta>alas") {
+    ylhaaltaAlas(lomake);
   }
+});
+
+// Siirrä elementti vasemmalta oikealle
+function vasenOikea(lomake) {
+  const alkupiste = -lomake.offsetWidth;
+  const loppupiste = 100;
+  lomake.style.left = `${alkupiste}px`;
+
+  setTimeout(() => {
+    lomake.style.transition = "left .8s";
+    lomake.style.left = `${loppupiste}px`;
+  }, 0);
+}
 
 // Siirrä elementti ylhäältä alas
 function ylhaaltaAlas(lomake) {
-    const alkupiste = -lomake.offsetHeight - 120;
-    const loppupiste = 20;
-    lomake.style.top = `${alkupiste}px`;
-    lomake.style.left = "100px";
+  const alkupiste = -lomake.offsetHeight - 120;
+  const loppupiste = 20;
+  lomake.style.top = `${alkupiste}px`;
+  lomake.style.left = "100px";
 
-    setTimeout(() => {
+  setTimeout(() => {
     lomake.style.transition = "top 0.8s";
     lomake.style.top = `${loppupiste}px`;
-}, 0);
+  }, 0);
 }
